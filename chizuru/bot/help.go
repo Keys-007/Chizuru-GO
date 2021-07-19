c242b5985c69f60a2b1745ee62ef2e9a8cee9a89
Sayan Biswas <sayan@pokurt.me>
2021-07-19 15:41:52 +0530

    Help: rework
    Signed-off-by: Sayan Biswas <sayan@pokurt.me>

--- chizuru/bot/help.go
+++ chizuru/bot/help.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/PaulSonOfLars/gotgbot/v2"
 	"github.com/PaulSonOfLars/gotgbot/v2/ext"
+	"strings"
 )
 
 var NekosHelpStr = fmt.Sprintf(`
@@ -18,12 +19,40 @@ Supported endpoints:
 %s
 `, nekosAPI.GetEndpointsHTML())
 
+var BotHelpSTR = `
+<b>Hi there, I'm %s, I fetch lewds, just for you!</b>
+
+Currently I have these modules, 
+do /help modname to know about em.
+➨ <code>nekos</code>
+➨ <code>e621</code>
+
+`
+
+var e621HelpStr = `
+<b>This is the help for the e621 package</b>.
+
+This makes me send furry posts fetched from <code>e621.net</code>
+
+To use, send <code>/e621 tag</code>
+Example: <code>/e621 fox</code>`
+
 func getHelp(b *gotgbot.Bot, ctx *ext.Context) (err error) {
 	if ctx.EffectiveChat.Type == "private" {
-		_, err = ctx.EffectiveMessage.Reply(b, NekosHelpStr, &gotgbot.SendMessageOpts{ParseMode: "html"})
-		return err
+		args := ctx.Args()
+		if len(args) == 1 {
+			_, err = ctx.EffectiveMessage.Reply(b, fmt.Sprintf(BotHelpSTR, b.User.FirstName), &gotgbot.SendMessageOpts{ParseMode: "html"})
+			return err
+		} else if strings.ToLower(args[1]) == "nekos" {
+			_, err = ctx.EffectiveMessage.Reply(b, NekosHelpStr, &gotgbot.SendMessageOpts{ParseMode: "html"})
+			return err
+		} else if strings.ToLower(args[1]) == "e621" {
+			_, err = ctx.EffectiveMessage.Reply(b, e621HelpStr, &gotgbot.SendMessageOpts{ParseMode: "html"})
+			return err
+		}
 	} else {
 		_, err = ctx.EffectiveMessage.Reply(b, "Command restricted to PM.", &gotgbot.SendMessageOpts{ParseMode: "html"})
 		return err
 	}
+	return err
 }
